b0d338d8c8b59ef1668d3a333c066ce35718c727
Nataly Ramirez Urrego <nataly.ramirez@datalytics.com>
2023-02-28 11:25:05 -0500

    Script de consulta versión final

--- scriptdeconsulta.py
+++ scriptdeconsulta.py
@@ -1,4 +1,4 @@
-from analisisrentabilidades.procesamiento import limpieza_datos_retabilidades
+from analisisrentabilidades.procesamiento import limpieza_datos_retabilidades, act_df_rentabilidades
 from analisisrentabilidades.consultas import consultanit, result_rentabilidades
 
 Lista_fondos = "https://fiduciaria.grupobancolombia.com/consultarFondosInversion/rest/servicio/consultarListaFondos"
@@ -10,9 +10,11 @@ if __name__ == "__main__":
      print("Inicio de consulta Nits")
      Nits= consultanit(Lista_fondos)
      print("Extraer rentabilidades de los fondos")
-     Fond= result_rentabilidades(url_base)
+     Fond= result_rentabilidades(url_base, Nits)
      print("Limpieza de dataset final")
      DFfondos= limpieza_datos_retabilidades(Fond)
-     print("Escritura de resultados")
      ruta= 'Fondos.csv'
-     Archivo= DFfondos.to_csv(ruta, index=False)
+     print("Actualización de archivo histórico")
+     act_df_rent = act_df_rentabilidades(ruta,DFfondos)
+     print("Escritura de resultados")
+     act_df_rent.to_csv(ruta, index=False)
