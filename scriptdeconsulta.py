cdf60ea5b6c7df3e5bb04af9c73fc994c640438e
Nataly Ramirez Urrego <nataly.ramirez@datalytics.com>
2023-03-23 12:54:56 -0500

    escritura en el DataLake

--- scriptdeconsulta.py
+++ scriptdeconsulta.py
@@ -1,38 +1,40 @@
 from analisisrentabilidades.procesamiento import limpieza_datos_retabilidades, act_df_rentabilidades, escribir_resultados
 from analisisrentabilidades.consultas import consultanit, result_rentabilidades
 from analisisrentabilidades.datalake import InteractuarDataLake
+from azure.storage.blob import BlobServiceClient
 
 Lista_fondos = "https://www.bancolombia.com/consultarFondosInversion/rest/servicio/consultarListaFondos"
 url_base =  "https://www.bancolombia.com/consultarFondosInversion/rest/servicio/buscarInformacionFondo/"
 
-# ACCOUNT_NAME = "datalakecienciadatos"
-# ACCOUNT_KEY = "GjeaiEjBTpU9uCe+WcVip8kPQOyIesAxy6kvv9drKLRTgGuLVwrocehT4lowSaHi4ZT8MjWhnbth+AStdqP6/w=="
+ACCOUNT_NAME = "datalakecienciadatos"
+ACCOUNT_KEY = "GjeaiEjBTpU9uCe+WcVip8kPQOyIesAxy6kvv9drKLRTgGuLVwrocehT4lowSaHi4ZT8MjWhnbth+AStdqP6/w=="
+
 
 if __name__ == "__main__": 
      print("Inicio de consulta Nits")
      Nits= consultanit(Lista_fondos)
+
      print("Extraer rentabilidades de los fondos")
      Fond= result_rentabilidades(url_base, Nits)
+     
      print("Limpieza de dataset final")
      DFfondos= limpieza_datos_retabilidades(Fond)
+     
+     print("Lectura archivo DataLake")
      ruta= 'Fondos.csv'
+     ClaseDL = InteractuarDataLake(ACCOUNT_NAME,ACCOUNT_KEY)
+     CONTAINER_NAME = "proyectofondos"
+     ClaseDL.blob_client()
+     ClaseDL.lectura_container(ruta, CONTAINER_NAME,ruta)
+     
      print("Actualización de archivo histórico")
      act_df_rent = act_df_rentabilidades(ruta,DFfondos)
+    
      print("Escritura de resultados")
      escribir_resultados(ruta, act_df_rent)
+     
+     ClaseDL.escritura_container(ruta,CONTAINER_NAME,ruta)
+     rutalog='log_ejecuciones.csv'
 
-     # CONTAINER_NAME = "proyectofondos"
-     # BLOB_NAME = "Fondos.csv"    
-
-     # Interaccion con el datalake
-     # Definición de la clase
-     # ClaseDL = InteractuarDataLake(ACCOUNT_NAME,ACCOUNT_KEY)
-     # ClaseDL.blob_client()
-
-     # # Escritura del archivo
-     # ClaseDL.escritura_container("Fondos.csv",CONTAINER_NAME, BLOB_NAME)
-
-     # escritura desde el datalake a la mv
-     # download_file_path = "Nuevo_archivo2.csv"
-     # file_path = 'Fondos.csv'
-     # ClaseDL.lectura_container(download_file_path,CONTAINER_NAME,file_path)
+     ClaseDL.lectura_container(rutalog, CONTAINER_NAME,rutalog)
+     ClaseDL.escritura_container(rutalog,CONTAINER_NAME,rutalog)
